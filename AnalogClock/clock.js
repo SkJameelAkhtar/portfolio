9b9690b3896d383124aff8ea7683e549ed630934
Sk Jameel Akhtar <76216831+SkJameelAkhtar@users.noreply.github.com>
2023-01-12 20:05:21 +0530

    Add files via upload

---
+++ AnalogClock/clock.js
@@ -0,0 +1,14 @@
+function clockdata(){
+    time=new Date;
+    shand=time.getSeconds();
+    mhand=time.getMinutes();
+    hrhand=time.getHours();
+    sr=shand*6;
+    mr=mhand*6;
+    hr=(hrhand*30 + (mhand/2));
+
+    sechand.style.transform = `rotate(${sr}deg)`;
+    minhand.style.transform = `rotate(${mr}deg)`;
+    hhand.style.transform = `rotate(${hr}deg)`;
+}
+setInterval(clockdata,1000);
