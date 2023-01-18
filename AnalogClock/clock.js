a7b7a8d6bd0c672cfe097fed42d46aa742c0ac17
Sk Jameel Akhtar <76216831+SkJameelAkhtar@users.noreply.github.com>
2023-01-18 20:37:27 +0530

    Update clock.js

--- AnalogClock/clock.js
+++ AnalogClock/clock.js
@@ -6,7 +6,7 @@ function clockdata(){
     sr=shand*6;
     mr=mhand*6;
     hr=(hrhand*30 + (mhand/2));
-
+    document.getElementById('timeloc').innerHTML = time;
     sechand.style.transform = `rotate(${sr}deg)`;
     minhand.style.transform = `rotate(${mr}deg)`;
     hhand.style.transform = `rotate(${hr}deg)`;
